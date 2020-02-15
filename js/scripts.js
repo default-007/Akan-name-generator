a75f5da8331c4e095c7cb3e0ee67853df68f02b4
Brian Otieno <brianokola@gmail.com>
2020-02-15 18:58:17 +0300

    Linked scripts.js to index.html and defined male & female akan names

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
-const maleAkanNames = {"Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
-const femaleAkanNames = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
+function calculateWeek(){
+  var maleAkanNames = ["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+  var femaleAkanNames = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  var day = parseInt(document.getElementById(day))
+  var month = parseInt(document.getElementById(month))
+  var year = parseInt(document.getElementById(year))
+  var yy = parseInt(year/100)
+  var cc = parseInt(year%100)
+
+  let dayofWeek =  ( ( (CC/4) -(2*CC)-1) + ((5*YY)/4) + ((26*(MM+1)/10)) + DD) % 7
+
+  if (gender === "male"){
+  alert("Your Akan Name is; " + maleAkanNames[dayofWeek] + " born on " + days[dayofWeek])
+  }else {
+  alert("Your Akan Name is; " + femaleAkanNames[dayofWeek] + " born on " + days[dayofWeek])
+  }
+  return false;
+  }
