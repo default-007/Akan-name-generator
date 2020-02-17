985e8987430dfd892cb56f658d37bfcf81ca110e
Brian Otieno <brianokola@gmail.com>
2020-02-17 14:23:48 +0300

    Delete extra  copies of files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-$(document).ready(function(){
-    $("#myBirthDate").mask("99/99/9999");
-});
-
 
 function getAkanName(){
     var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -12,31 +8,26 @@ function getAkanName(){
     var dateOfBirth = new Date(myBirthday);
     var dayOfTheWeek = dateOfBirth.getDay();
     if(myBirthday === ""){
-        document.getElementById('message').innerHTML = " You Didn't Submit a Valid Date!";
+        document.getElementById('message').innerHTML = "<div> You Didn't Submit a Valid Date!</div>";
 
     }
     else {
         for(var i=0;i<myGender.length;i++){
             if(myGender[i].checked){
                 if(myGender[i].value === "Male"){
-                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] +, "Your Akan Name's " + maleAkanNames[dayOfTheWeek] + "";
+                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
 
                 }
                 else {
-                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] + "Your Akan Name's" + femaleAkanNames[dayOfTheWeek] + "";
+                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + femaleAkanNames[dayOfTheWeek] + "</span>";
 
                 }
                 break;
             }
             else {
-                document.getElementById('message').innerHTML = "Please select a Gender";
+                document.getElementById('message').innerHTML = "<div>Please select a Gender</div>";
 
             }
         }
     }
-}
-
-function clearAkanMessage(){
-    document.getElementById('message').innerHTML = "";
-    return false;
 }
