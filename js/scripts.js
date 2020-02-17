b18bdd494a54b6fecc5674024e3cb673442fc3b3
Brian Otieno <brianokola@gmail.com>
2020-02-17 11:15:34 +0300

    Changed code to match the built in date system in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,30 @@
+function getAkanName(){
+  const maleAkanNames = ["Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+  const femaleAkanNames = ["Ama", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"];
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  //Collect data from html
+  var gender = document.getElementsByName("gender").value;
+  var myBirthday = document.getElementById("myBirthDate").value;
+  var dateOfBirth = new Date(myBirthday);
+  var dayOfTheWeek = dateOfBirth.getDay();
 
-  const maleAkanNames = ["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
-  const femaleAkanNames = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
-  const daysofWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  //collect data from html
-  var day = parseInt(document.getElementById("day").value);
-  var month = parseInt(document.getElementById("month").value)-1;
-  var year = parseInt(document.getElementById("year").value);
-  var gender = document.getElementsByName("gender");
-  //split year code and century code
-  var yy = parseInt(year%100);
-  var cc = parseInt(year/100);
-
-var dayofWeek = function calculateWeek(month, year, day){
-    return ( ( (cc/4) -(2*cc)-1) + ((5*yy)/4) + ((26*(month+1)/10)) + day) % 7;
+  if (myBirthday === ""){
+    document.getElementById('message').innerHTML = "Didn't submit valid date";
+    alert("Enter Birth Date");
+  }
+  else {
+    for (var i = 0; i < myGender.length; i++) {
+      if (myGender[i].checked){
+        if (myGender[i].value === "Male"){
+          alert("Born on a " + "" + days[dayOfTheWeek], + "Your Akan Name is" + "" + maleAkanNames[dayOfTheWeek]);
+        }
+        else {
+          alert("Born on a " + "" + days[dayOfTheWeek], + "Your Akan Name is" + "" + femaleAkanNames[dayOfTheWeek]);
+        }
+        break;
+      }
+      else {
+        document.getElementById('message').innerHTML = "Plase select a Gender";
+      }
+    }
 }
-if dayofWeek (day === 0) {
-  return "Sunday";
-} else if (day === 1) {
-  return "Monday";
-} else if (day === 2) {
-  return "Tuesday";
-} else if (day === 3) {
-  return "Wednesday";
-} else if (day === 4) {
-  return "Thursday";
-} else if (day === 5) {
-  return "Friday";
-} else (day === 6) {
-  return "Sartuday";
-}
-function akan()  {if (gender === "male"){
-  alert("Your Akan Name is; " + maleAkanNames[dayofWeek] + " born on " + daysofWeek[dayofWeek])
-} else(gender === "female") {
-  alert("Your Akan Name is; " + femaleAkanNames[dayofWeek] + " born on " + daysofWeek[dayofWeek])
-};
-S};
