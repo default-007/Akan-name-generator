9a249fce0580ce17d5811466fc400a49069af231
Brian Otieno <brianokola@gmail.com>
2020-02-17 05:22:56 +0300

    Assign index to days of week string

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,22 @@ function calculateWeek(){
   var yy = parseInt(year/100);
   var cc = parseInt(year%100);
 
-  var dayofWeek =  ( ( (CC/4) -(2*CC)-1) + ((5*YY)/4) + ((26*(MM+1)/10)) + DD) % 7;
-
+  var dayofWeek =  ( ( (cc/4) -(2*cc)-1) + ((5*yy)/4) + ((26*(month+1)/10)) + day) % 7;
+if day (day === 0) {
+  return "Sunday"
+} else if (day === 1) {
+  return "Monday"
+} else if (day === 2) {
+  return "Tuesday"
+} else if (day === 3) {
+  return "Wednesday"
+} else if (day === 4) {
+  return "Thursday"
+} else if (day === 5) {
+  return "Friday"
+} else (day === 6) {
+  return "Sartuday"
+}
   if (gender === "male"){
   alert("Your Akan Name is; " + maleAkanNames[dayofWeek] + " born on " + daysofWeek[dayofWeek])
 } else(gender === "female") {
