a6b3a4a45b5a5141c92d505b02f6edb5271056a3
Brian Otieno <brianokola@gmail.com>
2020-02-17 05:38:03 +0300

    changed indentation and spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
-function calculateWeek(){
+
   const maleAkanNames = ["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
   const femaleAkanNames = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
   const daysofWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  //collect data from html
   var day = parseInt(document.getElementById(day).value);
-  var month = parseInt(document.getElementById(month).value);
+  var month = parseInt(document.getElementById(month).value)-1;
   var year = parseInt(document.getElementById(year).value);
   var gender = document.getElementsByName(gender)
   var yy = parseInt(year%100);
   var cc = parseInt(year/100);
 
+function calculateWeek(){
   var dayofWeek =  ( ( (cc/4) -(2*cc)-1) + ((5*yy)/4) + ((26*(month+1)/10)) + day) % 7;
+]
 if day (day === 0) {
-  return "Sunday"
+  return "Sunday";
 } else if (day === 1) {
-  return "Monday"
+  return "Monday";
 } else if (day === 2) {
-  return "Tuesday"
+  return "Tuesday";
 } else if (day === 3) {
-  return "Wednesday"
+  return "Wednesday";
 } else if (day === 4) {
-  return "Thursday"
+  return "Thursday";
 } else if (day === 5) {
-  return "Friday"
+  return "Friday";
 } else (day === 6) {
-  return "Sartuday"
+  return "Sartuday";
 }
   if (gender === "male"){
   alert("Your Akan Name is; " + maleAkanNames[dayofWeek] + " born on " + daysofWeek[dayofWeek])
