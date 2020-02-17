3de55dbfe00e0576a2818d4e37c119011fc0008c
Brian Otieno <brianokola@gmail.com>
2020-02-17 09:34:37 +0300

    Change button type from submit to button

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
   const femaleAkanNames = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
   const daysofWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   //collect data from html
-  var day = parseInt(document.getElementById(day).value);
-  var month = parseInt(document.getElementById(month).value)-1;
-  var year = parseInt(document.getElementById(year).value);
-  var gender = document.getElementsByName(gender)
+  var day = parseInt(document.getElementById("day").value);
+  var month = parseInt(document.getElementById("month").value)-1;
+  var year = parseInt(document.getElementById("year").value);
+  var gender = document.getElementsByName("gender");
   var yy = parseInt(year%100);
   var cc = parseInt(year/100);
 
-var dayofWeek =function calculateWeek(month, year, day){
+var dayofWeek = function calculateWeek(month, year, day){
     return ( ( (cc/4) -(2*cc)-1) + ((5*yy)/4) + ((26*(month+1)/10)) + day) % 7;
 ]
 if dayofWeek (day === 0) {
@@ -28,8 +28,9 @@ if dayofWeek (day === 0) {
 } else (day === 6) {
   return "Sartuday";
 }
-  if (gender === "male"){
+function akan()  {if (gender === "male"){
   alert("Your Akan Name is; " + maleAkanNames[dayofWeek] + " born on " + daysofWeek[dayofWeek])
 } else(gender === "female") {
   alert("Your Akan Name is; " + femaleAkanNames[dayofWeek] + " born on " + daysofWeek[dayofWeek])
-};
+}
+;
